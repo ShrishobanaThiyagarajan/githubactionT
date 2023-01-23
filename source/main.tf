a4d257a26bc3c43d5a5e36fedae3b88a1d5c70ba
Gøran Hansen <mail@goeran.no>
2023-01-23 23:46:02 +0100

    Refactored: introduced module for azure func microservice

--- source/main.tf
+++ source/main.tf
@@ -1,27 +1,5 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.0"
-    }
-  }
+module "az-func-microservice" {
+    source = "./modules/az-func-microservice"
+    service_name = "hello-world"
+    github_token = var.github_token
 }
-
-# Configure the GitHub Provider
-provider "github" {
-    token = var.github_token
-    owner = "Karnov-Group-Norway"
-
-}
-
-/*resource "github_repository" "hello-world" {
-    name = "hello-world"
-    description = "goeran tester"
-    visibility = "private"
-
-    template {
-        owner = "Karnov-Group-Norway"
-        repository = "az-func-csharp-template"
-        include_all_branches = false
-  }
-}*/
---
+++ source/modules/az-func-microservice/main.tf
@@ -0,0 +1,81 @@
+terraform {
+  required_providers {
+    github = {
+        source  = "integrations/github"
+        version = "~> 5.0"
+    }
+
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "=3.40.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+    token = var.github_token
+    owner = "Karnov-Group-Norway"
+
+}
+
+resource "github_repository" "microservice_repository" {
+    name = var.service_name
+    description = "goeran tester"
+    visibility = "private"
+
+    template {
+        owner = "Karnov-Group-Norway"
+        repository = "az-func-csharp-template"
+        include_all_branches = false
+  }
+}
+
+resource "github_repository_file" "appsettings" {
+    repository          = github_repository.microservice_repository.name
+    branch              = "main"
+    file                = "source/Func/appsettings.json"
+    content             = templatefile("modules/az-func-microservice/appsettings.tftpl", { service_name = var.service_name })
+    commit_message      = "Managed by Terraform"
+    commit_author       = "Terraform User"
+    commit_email        = "terraform@example.com"
+    overwrite_on_create = true
+}
+
+provider "azurerm" {
+  features {}
+}
+
+/*resource "azurerm_resource_group" "example" {
+  name     = "goeran-test"
+  location = "Norway East"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storagegoerantestfunc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "goeran-test-func-service-plan"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                = "goeran-test-func"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  #service_plan_id            = azurerm_service_plan.example.id
+  service_plan_id = azurerm_service_plan.example.id
+
+  site_config {}
+}*/
