85cec11c205a1828272669fca3c5a4cf5aa02576
Gøran Hansen <mail@goeran.no>
2023-02-15 11:05:13 +0100

    Added client conf for azure

--- source/environments/dev/main.tf
+++ source/environments/dev/main.tf
@@ -19,6 +19,8 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "data-dev-k" {
   name     = "data-dev-k"
   location = "West Europe"
