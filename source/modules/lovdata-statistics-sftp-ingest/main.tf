92d9bff5921efc93a951a554d9fee88021316739
Gøran Hansen <mail@goeran.no>
2023-02-15 21:36:13 +0100

    Trying to add a local users

--- source/modules/lovdata-statistics-sftp-ingest/main.tf
+++ source/modules/lovdata-statistics-sftp-ingest/main.tf
@@ -32,3 +32,28 @@ resource "azurerm_storage_container" "lovdatastats" {
   storage_account_name  = azurerm_storage_account.sftpstorage.name
   container_access_type = "private"
 }
+
+resource "azurerm_storage_account_local_user" "lovdata" {
+  name                 = "ghtest"
+  storage_account_id   = azurerm_storage_account.sftpstorage.id
+  ssh_key_enabled      = true
+  ssh_password_enabled = true
+  home_directory       = "lovdatastats"
+  
+  #ssh_authorized_key {
+  #  description = "key1"
+  #  key         = local.first_public_key
+  #}
+  #ssh_authorized_key {
+  #  description = "key2"
+  #  key         = local.second_public_key
+  #}
+  permission_scope {
+    permissions {
+      read   = true
+      create = true
+    }
+    service       = "blob"
+    resource_name = azurerm_storage_container.lovdatastats.name
+  }
+}
