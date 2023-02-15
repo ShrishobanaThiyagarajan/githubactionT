f65db5fde98c3a7d16f9431e78d8264d3d68d1d0
Gøran Hansen <mail@goeran.no>
2023-02-15 12:16:04 +0100

    refactoring: fixed formatting

--- source/environments/prod/main.tf
+++ source/environments/prod/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "=3.40.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.40.0"
     }
   }
 
@@ -27,20 +27,20 @@ resource "azurerm_resource_group" "resourcegroup" {
 }
 
 resource "azurerm_key_vault" "keyvault" {
-  name                        = "kv-${lower(var.environment_name)}-k"
-  location                    = azurerm_resource_group.resourcegroup.location
-  resource_group_name         = azurerm_resource_group.resourcegroup.name
+  name                            = "kv-${lower(var.environment_name)}-k"
+  location                        = azurerm_resource_group.resourcegroup.location
+  resource_group_name             = azurerm_resource_group.resourcegroup.name
   enabled_for_template_deployment = true
-  enabled_for_disk_encryption = false
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 90
-  purge_protection_enabled    = false
+  enabled_for_disk_encryption     = false
+  tenant_id                       = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days      = 90
+  purge_protection_enabled        = false
 
   sku_name = "standard"
 }
 
 module "lovdata-statistics-sftp-ingest" {
-  source = "../../modules/lovdata-statistics-sftp-ingest"
+  source              = "../../modules/lovdata-statistics-sftp-ingest"
   resource_group_name = azurerm_resource_group.resourcegroup.name
-  environment_name = var.environment_name
+  environment_name    = var.environment_name
 }
