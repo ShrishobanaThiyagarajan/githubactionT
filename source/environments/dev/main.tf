cbdb8eba40122e573e21a3c4d92ef41a4a296748
Gøran Hansen <mail@goeran.no>
2023-04-15 20:40:29 +0200

    Deleted the ContentReports microservice

--- source/environments/dev/main.tf
+++ source/environments/dev/main.tf
@@ -78,12 +78,3 @@ output "lovdata_statistics_sftp" {
   sensitive = true
   value     = module.lovdata-statistics-sftp-ingest
 }
-
-module "az_func_microservice_ContentReports" {
-    source = "../../modules/az-func-microservice"
-    service_name = "ContentReports"
-    github_token = var.github_token
-    environment_name = var.environment_name
-    resource_group_name = azurerm_resource_group.resourcegroup.name
-}
-
