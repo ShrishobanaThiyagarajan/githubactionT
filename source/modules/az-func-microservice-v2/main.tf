3377fc7bec0dbe9de058c03f1bf22309c6228cb7
Gøran Hansen <mail@goeran.no>
2023-04-17 14:04:49 +0200

    windows func app

--- source/modules/az-func-microservice-v2/main.tf
+++ source/modules/az-func-microservice-v2/main.tf
@@ -34,6 +34,24 @@ resource "azurerm_storage_account" "func_storage_account" {
   }
 }
 
+resource "azurerm_windows_function_app" "windows_func" {
+  name                = var.service_name
+  resource_group_name = var.func_resource_group_name
+  location            = var.func_resource_group_location
+
+  storage_account_name = azurerm_storage_account.func_storage_account.name
+  storage_account_access_key = azurerm_storage_account.func_storage_account.primary_access_key
+  service_plan_id = azurerm_service_plan.service_plan.id
+
+  site_config {
+
+  }
+
+  tags = {
+    environment = var.environment_name
+  }
+}
+
 
 /*resource "azurerm_resource_group" "example" {
   name     = "goeran-test"
