f29450ca37acda171490b88b9ea718c64d8e9cd6
Gøran Hansen <mail@goeran.no>
2023-04-17 14:10:07 +0200

    Deleted dead code

--- source/modules/az-func-microservice-v2/main.tf
+++ source/modules/az-func-microservice-v2/main.tf
@@ -51,38 +51,3 @@ resource "azurerm_windows_function_app" "windows_func" {
     environment = var.environment_name
   }
 }
-
-
-/*resource "azurerm_resource_group" "example" {
-  name     = "goeran-test"
-  location = "Norway East"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "storagegoerantestfunc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_service_plan" "example" {
-  name                = "goeran-test-func-service-plan"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  os_type             = "Linux"
-  sku_name            = "Y1"
-}
-
-resource "azurerm_linux_function_app" "example" {
-  name                = "goeran-test-func"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  #service_plan_id            = azurerm_service_plan.example.id
-  service_plan_id = azurerm_service_plan.example.id
-
-  site_config {}
-}*/
