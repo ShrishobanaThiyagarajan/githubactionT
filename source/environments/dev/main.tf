e20413bb52b76a1355015aebef662f72c1807c36
kuldesjokk <nichlas.tveit.schmidt@karnovgroup.no>
2023-12-01 12:56:43 +0100

    Created Nts hello world project

--- source/environments/dev/main.tf
+++ source/environments/dev/main.tf
@@ -924,6 +924,27 @@ module "microservice_OsaHelloWorld" {
   teams_incoming_webhooks_url_prod = var.teams_incoming_webhooks_url_prod
 }
 
+module "microservice_NtsHelloWorld" {
+  source       = "../../modules/az-func-microservice-v2"
+  service_name = "NtsHelloWorld"
+  funcs = [
+    {
+      service_name = "NtsHelloWorld",
+      proj_path    = "./source/KarnovN.NtsHelloWorld.Func/KarnovN.NtsHelloWorld.Func.csproj"
+    }
+  ]
+  func_resource_group_name         = "functions-${lower(var.environment_name)}-k"
+  environment_name                 = var.environment_name
+  github_token                     = var.github_token
+  provision_repository             = true
+  sln_path                         = "./NtsHelloWorld.sln"
+  sonarcloud_token                 = data.azurerm_key_vault_secret.notification_sonarcloud_token.value
+  azure_credentials_test           = var.azure_credentials_test
+  azure_credentials_prod           = var.azure_credentials_prod
+  teams_incoming_webhooks_url_test = var.teams_incoming_webhooks_url_test
+  teams_incoming_webhooks_url_prod = var.teams_incoming_webhooks_url_prod
+}
+
 output "lovdata_statistics_sftp" {
   sensitive = true
   value     = module.lovdata-statistics-sftp-ingest
