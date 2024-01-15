c73ec747a41295c9f2279bea5bece9fd043c750e
Odd Are Sæhle <odd.are.saehle@karnovgroup.no>
2024-01-15 07:16:24 +0100

    Added LLMIntegration (#121)

--- source/environments/dev/main.tf
+++ source/environments/dev/main.tf
@@ -1004,6 +1004,27 @@ data "azurerm_key_vault_secret" "goerandemo_sonarcloud_token" {
   key_vault_id = azurerm_key_vault.keyvault.id
 }
 
+module "microservice_LLMIntegration" {
+  source       = "../../modules/az-func-microservice-v2"
+  service_name = "LLMIntegration"
+  funcs = [
+    {
+      service_name = "LLMIntegration",
+      proj_path    = "./source/KarnovN.LLMIntegration.Func/KarnovN.LLMIntegration.Func.csproj"
+    }
+  ]
+  func_resource_group_name         = "functions-${lower(var.environment_name)}-k"
+  environment_name                 = var.environment_name
+  github_token                     = var.github_token
+  provision_repository             = true
+  sln_path                         = "./source/LLMIntegration.sln"
+  sonarcloud_token                 = data.azurerm_key_vault_secret.publishinginfo_sonarcloud_token.value
+  azure_credentials_test           = var.azure_credentials_test
+  azure_credentials_prod           = var.azure_credentials_prod
+  teams_incoming_webhooks_url_test = var.teams_incoming_webhooks_url_test
+  teams_incoming_webhooks_url_prod = var.teams_incoming_webhooks_url_prod
+}
+
 module "microservice_GoeranDemo" {
   source       = "../../modules/az-func-microservice-v2"
   service_name = "GoeranDemo"
