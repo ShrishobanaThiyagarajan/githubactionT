21a9d39da833d691438fd9140f9a11752a89264e
Nichlas <116650283+kuldesjokk@users.noreply.github.com>
2024-08-05 14:29:44 +0200

    removed duplicate module

--- source/environments/prod/main.tf
+++ source/environments/prod/main.tf
@@ -303,13 +303,6 @@ module "microservice_AuthorContract" {
     }
   ]
 }
-module "az_func_microservice_AuthorContract" {
-  source              = "../../modules/az-func-microservice"
-  service_name        = "AuthorContract"
-  github_token        = var.github_token
-  environment_name    = var.environment_name
-  resource_group_name = azurerm_resource_group.resourcegroup.name
-}
 
 module "microservice_InvoiceBasis" {
   source                   = "../../modules/az-func-microservice-v2"
