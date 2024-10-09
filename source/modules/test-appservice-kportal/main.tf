6e2b4e3d92fc2131e996535c1a78afc4913cb894
shrishobana <rshrishobana@gmail.com>
2024-10-09 15:24:49 +0200

    Added Terraform configurations for KPortal App Service

--- source/modules/test-appservice-kportal/main.tf
+++ source/modules/test-appservice-kportal/main.tf
@@ -1,81 +1,55 @@
+# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.52.0"
+      version = "~> 3.0.0"
     }
   }
+  required_version = ">= 0.14.9"
 }
-
 provider "azurerm" {
   features {}
 }
 
-module "repository" {
-  source                           = "../karnov-github"
-  provision_repository             = var.provision_repository
-  service_name                     = var.service_name
-  github_token                     = var.github_token
-  projs                            = var.apps
-  build_and_release_nuget          = false
-  sln_path                         = var.sln_path
-  sonarcloud_token                 = var.sonarcloud_token
-  azure_credentials_test           = var.azure_credentials_test
-  azure_credentials_prod           = var.azure_credentials_prod
-  teams_incoming_webhooks_url_test = var.teams_incoming_webhooks_url_test
-  teams_incoming_webhooks_url_prod = var.teams_incoming_webhooks_url_prod
+# Generate a random integer to create a globally unique name
+resource "random_integer" "kportalapp" {
+  min = 10000
+  max = 99999
 }
 
-data "azurerm_resource_group" "appservice_resource_group" {
-  name = var.appservice_resource_group_name
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup-${random_integer.ri.result}"
+  location = "eastus"
 }
 
-resource "azurerm_service_plan" "service_plan" {
-  count                  = length(var.apps)
-  name                   = var.appservice_serviceplan_name != "" ? var.appservice_serviceplan_name : "${lower(var.service_name)}-sp-${lower(var.environment)}-k"
-  resource_group_name    = data.azurerm_resource_group.appservice_resource_group.name
-  location               = data.azurerm_resource_group.appservice_resource_group.location
-  os_type                = "Windows"
-  sku_name               = var.service_plan_sku
-  zone_balancing_enabled = false
-  worker_count           = 1
+# Create the Windows App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "webapp-asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Windows"
+  sku_name            = "B1"
+}
 
-  tags = {
-    environment = var.environment
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "webapp-${random_integer.ri.result}"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
   }
 }
 
-resource "azurerm_windows_web_app" "windows_appservice" {
-  lifecycle {
-    ignore_changes = [
-      logs,
-    ]
-  }
-
-  count               = length(var.apps)
-  name                = var.appservice_name != "" ? var.appservice_name : "${lower(var.service_name)}-${lower(var.environment)}-k"
-  resource_group_name = data.azurerm_resource_group.appservice_resource_group.name
-  location            = data.azurerm_resource_group.appservice_resource_group.location
-  service_plan_id     = azurerm_service_plan.service_plan[count.index].id
-
-  site_config {
-    always_on                 = true
-    dotnet_framework_version  = "v6.0" # Adjust based on your .NET version
-    scm_type                  = "LocalGit" # If you plan to use Git for deployment
-  }
-
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
-    "ASPNETCORE_ENVIRONMENT"   = var.environment
-    "KPORTAL_BFF_URL"          = var.bff_url # Define this in variables.tf
-    # Add other necessary app settings here
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    environment = var.environment
-  }
+#  Deploy code from a public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.webapp.id
+  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+  branch             = "master"
+  use_manual_integration = true
+  use_mercurial      = false
 }
