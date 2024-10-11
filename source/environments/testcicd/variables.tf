b4e4e42bea914cc5c8e17f37e2c8239269ff4e25
shrishobana <rshrishobana@gmail.com>
2024-10-11 10:23:59 +0200

    1801-feature-cicdbranch _ Created AppService Terraform base upon KPortal

--- source/environments/testcicd/variables.tf
+++ source/environments/testcicd/variables.tf
@@ -1,3 +1,27 @@
-variable "environment_name" {
-    default = "testcicd"
+variable "github_token" {
+  default = "ADD-YOUR-OWN"
 }
+
+variable "environment_name" {
+  default = "testcicd"
+}
+
+variable "azure_credentials_test" {
+  type = string
+}
+
+variable "azure_credentials_prod" {
+  type = string
+}
+
+variable "teams_incoming_webhooks_url_test" {
+  type      = string
+  sensitive = true
+}
+
+variable "teams_incoming_webhooks_url_prod" {
+  type      = string
+  sensitive = true
+}
+
+
